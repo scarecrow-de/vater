8113d596b504c3b9ee1f9f6ba4157e7bba395d69
Microwave <121560464+Micro856@users.noreply.github.com>
2026-05-04 21:50:46 +0200

    revert some schemas back to gnome

--- src/core/prefs.c
+++ src/core/prefs.c
@@ -64,12 +64,12 @@
 
 /* These are the different schemas we are keeping
  * a GSettings instance for */
-#define SCHEMA_GENERAL         "io.github.scarecrow_de.desktop.wm.preferences"
+#define SCHEMA_GENERAL         "org.gnome.desktop.wm.preferences"
 #define SCHEMA_MUTTER          "io.github.scarecrow_de.mutter"
-#define SCHEMA_INTERFACE       "io.github.scarecrow_de.desktop.interface"
+#define SCHEMA_INTERFACE       "org.gnome.desktop.interface"
 #define SCHEMA_INPUT_SOURCES   "io.github.scarecrow_de.desktop.input-sources"
 #define SCHEMA_XSETTINGS       "io.github.scarecrow_de.settings-daemon.plugins.xsettings"
-#define SCHEMA_MOUSE           "io.github.scarecrow_de.settings-daemon.peripherals.mouse"
+#define SCHEMA_MOUSE           "org.gnome.settings-daemon.peripherals.mouse"
 
 #define SETTINGS(s) g_hash_table_lookup (settings_schemas, (s))
 
